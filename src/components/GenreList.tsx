427ff07100adda86954ba5ab6660b9c410227685
Jester Ryan Tanada <jestertanada03@gmail.com>
2025-04-01 20:26:21 +0800

    Filter games by genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,9 +1,20 @@
-import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
-import useGenres from "../hooks/useGenres";
+import {
+  Button,
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
+import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 import GenreSkeleton from "./GenreSkeleton";
 
-const GenreList = () => {
+interface Props {
+  onSelectGenre: (genre: Genre) => void;
+}
+const GenreList = ({ onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
   if (error) return null;
   //   if (isLoading) return <Spinner />;
@@ -12,9 +23,9 @@ const GenreList = () => {
     <>
       {isLoading &&
         skeletons.map((skeleton) => (
-          <HStack>
+          <HStack key={skeleton}>
             {" "}
-            <GenreSkeleton key={skeleton} />
+            <GenreSkeleton />
           </HStack>
         ))}
       <List>
@@ -26,7 +37,13 @@ const GenreList = () => {
                 borderRadius={8}
                 src={getCroppedImageUrl(genre.image_background)}
               />
-              <Text fontSize="lg">{genre.name}</Text>
+              <Button
+                onClick={() => onSelectGenre(genre)}
+                variant="link"
+                fontSize="lg"
+              >
+                {genre.name}
+              </Button>
             </HStack>
           </ListItem>
         ))}
--- src/components/GenreSkeleton.tsx
+++ src/components/GenreSkeleton.tsx
@@ -13,11 +13,8 @@ const GenreSkeleton = () => {
         {[...Array(5)].map((_, index) => (
           <ListItem key={index} paddingY="5px">
             <HStack>
-              {/* Skeleton for image */}
               <Skeleton height="32px" width="32px" borderRadius="8px" />
-
-              {/* Skeleton for text */}
-              <SkeletonText noOfLines={1} width="120px" spacing="4" />
+              <SkeletonText noOfLines={1} width="100px" spacing="4" />
             </HStack>
           </ListItem>
         ))}
